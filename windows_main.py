16d626ab2a6aaa3bd0c2524449f9dee72950ecb5
l0rex <104691158+TheLorexxx@users.noreply.github.com>
2022-09-09 22:28:49 +0300

    Update and rename main.py to windows_main.py

--- windows_main.py
+++ windows_main.py
@@ -2,9 +2,11 @@ import random
 import time
 import os
 
+os.system("color a")
 lorex = input("[1] - DDOS\n[2] - IPLOGGER\n[3] - SQL\n[4] - BYPASS\n[5] - BRUTEFORCE\n[6] - HAKKINDA\n\n>> ")
 
 if lorex == '1':
+    os.system("cls")
     print("DDOS Siteleri \n \n- https://stresser.app \n- https://www.ipstresser.com \n- https://str3ssed.co \n")
     print("[!] Bu ekran 30 saniye icinde kapanacaktir.")
     
@@ -12,26 +14,31 @@ if lorex == '1':
 
     
 if lorex == '2':
+    os.system("cls")
     print("IP Adresi nasil tespit edilir?\n\nIlk once yonlendirme adresi seciniz.\nOrnek : www.youtube.com\n\nGirdiginiz siteye yonlendirme linkini yaziniz.\nSize cesitli linkler verecektir bu linklerden birini kurbana atiniz.\nIP Adresi Siteye Dusecektir.\n- grafiby.link\n- iplogger.org")
     print("[!] Bu ekran 60 saniye icinde kapanacaktir.")
     time.sleep(60)
     
 if lorex == '3':
+    os.system("cls")
     print("SQL Acikli site bulma\n\nIlk once dork seciniz\nOrnek : php?id=\n\nBasina inurl: ekleyerek dorku aratiniz.\nRastgele bir siteye giriniz.\nSite adresinin sonuna tirnak isareti ekleyin.\nHata veriyorsa SQL aciklidir.\nSQLMAP veya baska bir sql injection toolu kullanarak siteyi hackleyebilirsiniz.")
     print("\n[!] Bu ekran 60 saniye icinde kapanacaktir.")
     time.sleep(60)
 
 if lorex == '4':
+    os.system("cls")
     print("BYPASS acikli site nasil tespit edilir?\nIlk once dork seciniz.\nOrnek : login.php\n\n Basina inurl: ekleyerek dorku aratiniz.\nRastgele bir siteye giriniz.\nAdmin panelde kullanici adi ve sifresine herhangi bir bypass yazarsak siteyi hacklemis olacagiz.\nHer sitede calismayabilir bilginize.\n")
     print("\n[!] Bu ekran 60 saniye icinde kapanacaktir.")
     time.sleep(60)
 
 if lorex == '5':
+    os.system("cls")
     print("BRUTEFORCE nasil kullanilir?\n\nBruteForce bir suru sey deneyerek sifre kirmaya vb. yarar.\nGunumuzde bu ihtimal cok dusuk.\n\nBruteForce 1.8 Skyfall programini indiriniz.\nPassword kismina denenecek sifreleri yaziniz.\nBruteForce saldirisini baslatiniz.\nCok dusuk bir ihtimal bile olsa sifresi kolay ise hesabi alabilirsiniz.")
     print("\n[!] Bu ekran 60 saniye icinde kapanacaktir.")
     time.sleep(60)
 
 if lorex == '6':
+    os.system("cls")
     print("\n                                  Coded By Lorex \n\n                           >> lorexofficiall.glitch.me <<\n\n                       [!] Bu Ekran 20 Saniye Sonra Kapanacaktir.")
     time.sleep(20)
 
